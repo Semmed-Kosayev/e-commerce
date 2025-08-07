e135332a0492e5ccea632b1b2c2839fc068ce4f6
Semmed Kosayev <semmedkosayevbusiness@gmail.com>
2025-08-07 17:10:31 +0400

    created kafka shared library

---
+++ kafka-shared-classes/src/main/java/az/semmed/kafkasharedclasses/KafkaSharedClassesApplication.java
@@ -0,0 +1,13 @@
+package az.semmed.kafkasharedclasses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSharedClassesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaSharedClassesApplication.class, args);
+    }
+
+}
---
+++ kafka-shared-classes/src/test/java/az/semmed/kafkasharedclasses/KafkaSharedClassesApplicationTests.java
@@ -0,0 +1,13 @@
+package az.semmed.kafkasharedclasses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSharedClassesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
