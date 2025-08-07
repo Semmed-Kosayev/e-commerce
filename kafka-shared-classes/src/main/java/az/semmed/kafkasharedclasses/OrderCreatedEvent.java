aacdb73f5495d8714df87b03829127975942dfcc
Semmed Kosayev <semmedkosayevbusiness@gmail.com>
2025-08-07 18:17:51 +0400

    made the shared class even better

--- kafka-shared-classes/src/main/java/az/semmed/kafkasharedclasses/OrderCreatedEvent.java
+++ kafka-shared-classes/src/main/java/az/semmed/kafkasharedclasses/OrderCreatedEvent.java
@@ -1,12 +1,17 @@
 package az.semmed.kafkasharedclasses;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 public record OrderCreatedEvent(
         String orderId,
-        String productId,
-        int quantity,
-        BigDecimal price,
+        List<OrderItem> items,
         String customerEmail
 ) {
+    public record OrderItem(
+            String productId,
+            int quantity,
+            BigDecimal price
+    ) {
+    }
 }
