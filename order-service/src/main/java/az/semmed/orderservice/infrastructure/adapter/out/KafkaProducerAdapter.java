db6a12fb447d77b9b9228aea5d72fa0d27cb2695
Semmed Kosayev <semmedkosayevbusiness@gmail.com>
2025-08-08 14:24:57 +0400

    added kafka adapter

---
+++ order-service/src/main/java/az/semmed/orderservice/infrastructure/adapter/out/KafkaProducerAdapter.java
@@ -0,0 +1,20 @@
+package az.semmed.orderservice.infrastructure.adapter.out;
+
+import az.semmed.kafkasharedclasses.order.OrderCreatedEvent;
+import az.semmed.orderservice.application.port.out.KafkaProducerPort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class KafkaProducerAdapter implements KafkaProducerPort {
+
+    private final static String TOPIC = "order-events-topic";
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    @Override
+    public void sendOrderCreatedEvent(OrderCreatedEvent orderCreatedEvent) {
+        kafkaTemplate.send(TOPIC, orderCreatedEvent.orderId(), orderCreatedEvent);
+    }
+}
