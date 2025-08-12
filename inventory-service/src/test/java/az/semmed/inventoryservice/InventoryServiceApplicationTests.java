53e064df62ac13208b2c470e6b9439444a4c6caa
Semmed Kosayev <semmedkosayevbusiness@gmail.com>
2025-08-12 21:27:37 +0400

    initialized inventory service

---
+++ inventory-service/src/test/java/az/semmed/inventoryservice/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package az.semmed.inventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
